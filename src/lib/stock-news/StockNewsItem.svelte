669e45dab187fc5221e6ac9ead894f288e5c5baa
Paola Dimova <paola.dimova@news.co.uk>
2021-12-22 17:21:46 +0200

    add StockNews component

---
+++ src/lib/stock-news/StockNewsItem.svelte
@@ -0,0 +1,88 @@
+<script lang="ts" context="module">
+	import Card, {
+		Content,
+		PrimaryAction,
+		Media,
+		MediaContent,
+		Actions,
+		ActionButtons,
+		ActionIcons
+	} from '@smui/card';
+	import Button, { Label } from '@smui/button';
+	import IconButton, { Icon } from '@smui/icon-button';
+	import Chip, { Set, Text } from '@smui/chips';
+
+	export let title = '';
+	export let text = '';
+	export let source_name = '';
+	export let date = '';
+	export let sentiment = '';
+</script>
+
+<div class="card-display">
+	<div class="card-container">
+		<Card class="msa-card">
+			<!-- <PrimaryAction> -->
+			<Media class="card-media-16x9 msa-card-media" aspectRatio="16x9" />
+			<div class="msc-card-content-and-action">
+				<Content class="mdc-typography--body2">
+					<h2 class="mdc-typography--headline6" style="margin: 0;">{title}</h2>
+					{text}
+				</Content>
+				<Actions class="msa-card-actions">
+					<Set chips={['NKE']} let:chip nonInteractive>
+						<Chip {chip}>
+							<Text>{chip}</Text>
+						</Chip>
+					</Set>|
+					<Text>{source_name}</Text> | <Text>{date}</Text> | <Text
+						>{sentiment}</Text
+					>
+					<!-- <ActionButtons>
+					  <Button>
+						<Label>Action</Label>
+					  </Button>
+					  <Button>
+						<Label>Another</Label>
+					  </Button>
+					</ActionButtons> -->
+					<ActionIcons>
+						<IconButton toggle aria-label="Add to favorites" title="Add to favorites">
+							<Icon class="material-icons" on>favorite</Icon>
+							<Icon class="material-icons">favorite_border</Icon>
+						</IconButton>
+						<IconButton class="material-icons" title="Share">share</IconButton>
+						<IconButton class="material-icons" title="More options">more_vert</IconButton>
+					</ActionIcons>
+				</Actions>
+			</div>
+
+			<!-- </PrimaryAction> -->
+		</Card>
+	</div>
+</div>
+
+<style>
+	* :global(.card-media-16x9) {
+		background-image: url(https://place-hold.it/320x180?text=16x9&fontsize=23);
+	}
+
+	:global(.msa-card) {
+		display: flex;
+		flex-direction: row;
+	}
+
+	:global(.msa-card-media) {
+		border: 1px solid red;
+		flex: 1;
+	}
+
+	:global(.msc-card-content-and-action) {
+		flex: 2;
+	}
+
+	:global(.msa-card-actions) {
+		/* flex-direction: column; */
+		align-items: unset;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,60 @@
-<script context="module">
-	export const prerender = true;
-</script>
+<script lang="ts" context="module">
+	import Card, {
+		Content,
+		PrimaryAction,
+		Media,
+		MediaContent,
+		Actions,
+		ActionButtons,
+		ActionIcons
+	} from '@smui/card';
+	import Button, { Label } from '@smui/button';
+	import IconButton, { Icon } from '@smui/icon-button';
+	import Chip, { Set, Text } from '@smui/chips';
 
-<script>
-	import Counter from '$lib/Counter.svelte';
+	import StockNews from '../lib/stock-news/StockNews.svelte';
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
+<Button>
+	<Label>Default</Label>
+</Button>
 
-		to your new<br />SvelteKit app
-	</h1>
+<Button variant="raised">
+	<Label>Raised</Label>
+</Button>
 
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
+<StockNews/>
 
-	<Counter />
-</section>
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
+<!--
+  You need to import `@material/typography/mdc-typography` in SCSS for
+  these classes to work. Check out "_Typography.scss"
+-->
 
-	h1 {
-		width: 100%;
-	}
+<!-- <div class="mdc-typography--headline1">Headline 1</div>
+<div class="mdc-typography--headline2">Headline 2</div>
+<div class="mdc-typography--headline3">Headline 3</div>
+<div class="mdc-typography--headline4">Headline 4</div>
+<div class="mdc-typography--headline5">Headline 5</div>
+<div class="mdc-typography--headline6">Headline 6</div>
+<div class="mdc-typography--subtitle1">Subtitle 1</div>
+<div class="mdc-typography--subtitle2">Subtitle 2</div>
+<div class="mdc-typography--body1">Body 1</div>
+<div class="mdc-typography--body2">Body 2</div>
+<div class="mdc-typography--caption">Caption</div>
+<div class="mdc-typography--button">Button</div>
+<div class="mdc-typography--overline">Overline</div>
+<div class="mdc-typography--body1">
+	<small>small</small>
+	<big>big</big>
+	<sup>sup</sup>
+	<sub>sub</sub>
+	<strong>strong</strong>
+	<em>em</em>
+</div> -->
 
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<!-- <style></style> -->
